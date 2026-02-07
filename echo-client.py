782e557b6f102ffcd0eba4601c3c1223054201e1
FudgePizza <aviyg2@gmail.com>
2026-02-07 14:57:59 -0600

    Update echo-client.py

--- echo-client.py
+++ echo-client.py
@@ -1,10 +1,15 @@
 import socket
 import time
+import serial
 
 HOST = "192.168.1.101"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 buffer = b""
+
+ser = serial.Serial('COM5', 9600, timeout=1)  # Update with your port
+
+time.sleep(2)  # Wait for connection to establish
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
@@ -19,6 +24,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
             while b"\n" in buffer:
                 line, buffer = buffer.split(b"\n", 1)
-                print("Received:", line)
-        if line == b'close':
-            break
+                ser.write(line + b"\n")
+
+                if line == b"close":
+                    break
+            if line == b"close":
+                s.close()
+                break
