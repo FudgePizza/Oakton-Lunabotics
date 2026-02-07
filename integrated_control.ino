951d5c7eb6da80c021480db9d44156d74473ad62
FudgePizza <aviyg2@gmail.com>
2026-02-07 14:58:50 -0600

    Update integrated_control.ino

--- integrated_control.ino
+++ integrated_control.ino
@@ -1,30 +1,25 @@
-/**********************************************************************
-  Product     : Freenove 4WD Car for Raspberry Pi Pico (W)
-  Auther      : www.freenove.com
-  Modification: 2023/07/10
-**********************************************************************/
 #include <Servo.h>
 
-void Motor_Move(leftSpd, rightSpd){
-  constrain(rightSpd, -500,500);
-  constrain(lefftSpd, -500,500);
+Servo Motor1;
+//Servo Motor2;
+//Servo Motor3;
+//Servo Motor4; 
+
+void Motor_Move(int leftSpd, int rightSpd){
+  rightSpd = constrain(rightSpd, -500,500);
+  leftSpd = constrain(leftSpd, -500,500);
   rightSpd += 1500;
   leftSpd += 1500;
   Motor1.writeMicroseconds(leftSpd);
-  Motor2.writeMicroseconds(rightSpd);
+  //Motor2.writeMicroseconds(rightSpd);
   
 }
-
-Servo Motor1;
-Servo Motor2;
-//Servo Motor3;
-//Servo Motor4; 
 
 void setup() {
   Serial.begin(9600);
   
   Motor1.attach(8);
-  Motor2.attach(9); 
+  //Motor2.attach(9); 
   //Motor3.attach(10); 
   //Motor4.attach(11); //Motor Initializes
 }
@@ -32,32 +27,34 @@ void setup() {
 void loop() {
   // Check if there’s data in the serial buffer
   if (Serial.available() > 0) {
+    Serial.println("\n");
     String command = Serial.readStringUntil('\n');  // Read until newline
     command.trim(); // Remove spaces or line breaks
 
     if (command.equalsIgnoreCase("forward")) {
-      Serial.println("Moving forward...");
-      Motor_Move(250, 250);  // Both sides forward
+      //Serial.println("Moving forward...");
+      Motor_Move(100, 100);  // Both sides forward
 
     } else if (command.equalsIgnoreCase("backward")) {
-      Serial.println("Moving backward...");
-      Motor_Move(-250, -250);  // Both sides reverse
+      //Serial.println("Moving backward...");
+      Motor_Move(-100, -100);  // Both sides reverse
 
     } else if (command.equalsIgnoreCase("left")) {
-      Serial.println("Turning left...");
+      //Serial.println("Turning left...");
       Motor_Move(-200, 200);  // Left motors reverse, right motors forward
 
     } else if (command.equalsIgnoreCase("right")) {
-      Serial.println("Turning right...");
+      //Serial.println("Turning right...");
       Motor_Move(200, -200);  // Left forward, right reverse
 
-    } else if (command.equalsIgnoreCase("stop")) {
-      Serial.println("Stopping...");
+    } else if (command.equalsIgnoreCase("stop") or command.equalsIgnoreCase("none")) {
+      //Serial.println("Stopping...");
       Motor_Move(0, 0);  // Stop all motors
 
     } else {
-      Serial.println("Unknown command. Try: forward / backward / left / right / stop");
+      //Serial.println("Unknown command. Try: forward / backward / left / right / stop");
       Motor_Move(0,0);
     }
+    Serial.println("CE\n");
   }
 }
