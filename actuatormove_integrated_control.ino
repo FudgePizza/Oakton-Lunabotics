3c5c1b1e83537964466cc6368c93224563355459
bebs-1 <101741262+bebs-1@users.noreply.github.com>
2026-05-02 14:20:16 -0500

    Refactor actuator control and command processing
    Refactor actuator control logic and improve command handling.

--- actuatormove_integrated_control.ino
+++ actuatormove_integrated_control.ino
@@ -11,170 +11,123 @@ class Actuator {
 
 Actuator actuator1;
 Actuator actuator2;
-Servo Motor1;
-Servo Motor2;
-Servo Motor3;
-Servo Motor4;
-
-void countSteps1() { actuator1.stepCount++; }
-void countSteps2() { actuator2.stepCount++; }
+volatile int direction1 = 0;
+volatile int direction2 = 0;
+void countSteps1() { actuator1.stepCount+= 1*direction1; }
+void countSteps2() { actuator2.stepCount+= 1*direction2; }
 
 int spd = 500;
+int agiPwr = 20; 
 
 int dig = 1; 
 
 float pulsesPerIn = 441.9;
+Servo agitator;
 
-void actuatorSet(Actuator &act, float pos){ 
-  float dis = pos - act.pos;
-  int direction = (dis > 0) ? 1 : -1;
-  int aSpd = (constrain(spd, -500,500))* (direction);
-  aSpd += 1500;
-  act.stepCount = 0;
-  long pulsesNeeded = (long)(fabs(dis)*pulsesPerIn); 
-  attachInterrupt(digitalPinToInterrupt(act.pin),
-                (&act == &actuator1) ? countSteps1 : countSteps2,
-                RISING);                       
-  unsigned long start = millis();
-  act.actuator.writeMicroseconds(aSpd);
-  while(true){
-    noInterrupts();
-    long steps = act.stepCount;
-    interrupts();
-
-    if (steps >= pulsesNeeded) break;
-    if (millis() - start > 15000) break;
-    
-    delayMicroseconds(100);
-    
-  }
-  act.pos += direction* ((float)act.stepCount/pulsesPerIn);
-  act.actuator.writeMicroseconds(1500 + direction*200);
-  delay(40);
-  act.actuator.writeMicroseconds(1500);
-  detachInterrupt(digitalPinToInterrupt(act.pin));
-} 
-
-void actuatorDig(Actuator &act, int mSpd, int aSpd){ 
-  
-  int direction = (aSpd > 0) ? 1 : -1;
-  aSpd = (constrain(aSpd, -500,500));
-  aSpd += 1500;
-  act.stepCount = 0;
-   
-  attachInterrupt(digitalPinToInterrupt(act.pin),
-                (&act == &actuator1) ? countSteps1 : countSteps2,
-                RISING);
-  unsigned long start = millis();
-  act.actuator.writeMicroseconds(aSpd);
-  Motor_Move(mSpd,mSpd);
-  while(true){
-    noInterrupts();
-    long steps = act.stepCount;
-    interrupts();
-
-    if (millis() - start > 15000) break;
-    
-    delayMicroseconds(100);
-    
-  }
-  act.pos += direction* ((float)act.stepCount/pulsesPerIn);
-  act.actuator.writeMicroseconds(1500 + direction*200);
-  delay(40);
-  act.actuator.writeMicroseconds(1500);
-  Motor_Move(0,0);
-  detachInterrupt(digitalPinToInterrupt(act.pin));
-} 
-
-
-
-void Motor_Move(int leftSpd, int rightSpd){
-  rightSpd = constrain(rightSpd, -500,500);
-  leftSpd = constrain(leftSpd, -500,500);
-  rightSpd += 1500;
-  leftSpd += 1500;
-  Motor1.writeMicroseconds(leftSpd);
-  Motor2.writeMicroseconds(leftSpd);
-  Motor3.writeMicroseconds(rightSpd);
-  Motor4.writeMicroseconds(rightSpd);
-}
 
 void setup() {
   // put your setup code here, to run once:
+  agitator.attach(7);
+  agitator.writeMicroseconds(1500);
   Serial.begin(9600);
   actuator1.pin = 2;
-  pinMode(actuator1.pin,INPUT); // Hall effect sensor of Actuator 1
-  actuator1.actuator.attach(12);
+  pinMode(actuator1.pin,INPUT_PULLUP); // Hall effect sensor of Actuator 1 
+  actuator1.actuator.attach(12); // SM OUT Actuator 1 (Lift)
 
   actuator2.pin = 3;
-  pinMode(actuator2.pin,INPUT); // Hall effect sensor of Actuator 2
-  actuator2.actuator.attach(11);
+  pinMode(actuator2.pin,INPUT_PULLUP); // Hall effect sensor of Actuator 2
+  actuator2.actuator.attach(11);// SM OUT Actuator 2 (Tilt)
   
-  Motor1.attach(10);
-  Motor2.attach(9);
-  Motor3.attach(8);
-  Motor4.attach(7);
-
-  Motor1.writeMicroseconds(1500);
-  Motor2.writeMicroseconds(1500);
-  Motor3.writeMicroseconds(1500);
-  Motor4.writeMicroseconds(1500);
-  
-  actuator1.actuator.writeMicroseconds(2000);
-  actuator2.actuator.writeMicroseconds(2000);
-  delay(10000);
+  /*actuator1.actuator.writeMicroseconds(2000);
+  actuator2.actuator.writeMicroseconds(1000);
+  delay(20000); */
   actuator1.actuator.writeMicroseconds(1500);
   actuator2.actuator.writeMicroseconds(1500);
   actuator1.pos = 0;
   actuator2.pos = 0;
-
-  
+  attachInterrupt(digitalPinToInterrupt(actuator1.pin), countSteps1, RISING);
+  attachInterrupt(digitalPinToInterrupt(actuator2.pin), countSteps2, RISING);
+  Serial.setTimeout(20);
 }
 
 void loop() {
   // Check if there’s data in the serial buffer
+  static unsigned long lastPrint = 0;
+
   if (Serial.available() > 0) {
     
     String command = Serial.readStringUntil('\n');  // Read until newline
     command.trim(); // Remove spaces or line breaks
-
-    if (command.equalsIgnoreCase("forward")) {
-      //Serial.println("Moving forward...");
-      Motor_Move(spd, spd);  // Both sides forward
-
-    } else if (command.equalsIgnoreCase("backward")) {
-      //Serial.println("Moving backward...");
-      Motor_Move(-1*spd, -1*spd);  // Both sides reverse
-
-    } else if (command.equalsIgnoreCase("left")) {
-      //Serial.println("Turning left...");
-      Motor_Move(-1*spd, spd);  // Left motors reverse, right motors forward
-
-    } else if (command.equalsIgnoreCase("right")) {
-      //Serial.println("Turning right...");
-      Motor_Move(spd, -1*spd);  // Left forward, right reverse
-
-    } else if (command.equalsIgnoreCase("dig")) {
-      //Serial.println("Stopping...");
-      if(dig == 1){
-        dig = 2;
-        actuatorDig(actuator1, 250, 500);
-      } else{
-        dig = 1; 
-        //move actuators as needed
-      } 
+    
+    if (command.equalsIgnoreCase("u1")) {
+      
+      direction1 = 1;
+      actuator1.actuator.writeMicroseconds(1000);
+    } else if (command.equalsIgnoreCase("d1")) {
+      
+      direction1 = -1;
+      actuator1.actuator.writeMicroseconds(2000);
+    } else if (command.equalsIgnoreCase("u2")) {
+        if (actuator2.pos <= 8.0) {
+          direction2 = 1;
+          actuator2.actuator.writeMicroseconds(2000);
+          agitator.writeMicroseconds(1500 + agiPwr);
+      } else {
+          actuator2.actuator.writeMicroseconds(1500);
+          direction2 = 0;
+          agitator.writeMicroseconds(1500);
+      }
+      
+      
+    } else if (command.equalsIgnoreCase("d2")) {
+      direction2 = -1;
+      actuator2.actuator.writeMicroseconds(1000);
+      agitator.writeMicroseconds(1500+agiPwr);
+    } else if (command.equalsIgnoreCase("set")) {
+      noInterrupts();
+      actuator1.stepCount = 0;
+      actuator2.stepCount = 0;
+      interrupts();
 
     }
-     else if (command.equalsIgnoreCase("stop") || command.equalsIgnoreCase("none")) {
-      //Serial.println("Stopping...");
-      Motor_Move(0, 0);  // Stop all motors
-
+     else if (command.equalsIgnoreCase("stop1") || command.equalsIgnoreCase("none")) {
+      actuator1.actuator.writeMicroseconds(1500);
+      agitator.writeMicroseconds(1500);
+      direction1 = 0;
+    }
+    else if (command.equalsIgnoreCase("stop2") || command.equalsIgnoreCase("none")) {
+     
+      actuator2.actuator.writeMicroseconds(1500);
+      agitator.writeMicroseconds(1500);
+      direction2 = 0;
+    }
+    else if (command.equalsIgnoreCase("stop") || command.equalsIgnoreCase("none")) {
+      actuator1.actuator.writeMicroseconds(1500);
+      actuator2.actuator.writeMicroseconds(1500);
+      agitator.writeMicroseconds(1500);
+      direction1 = 0;
+      direction2 = 0; 
     }
     
-     else {
-      //Serial.println("Unknown command. Try: forward / backward / left / right / stop");
-      Motor_Move(0,0);
+     else { 
+      //actuator1.actuator.writeMicroseconds(1500);
+      //actuator2.actuator.writeMicroseconds(1500);
+      
     }
     
+    
+  }  
+  noInterrupts();
+  long steps1 = actuator1.stepCount;
+  long steps2 = actuator2.stepCount;
+  actuator1.pos = steps1 / pulsesPerIn;
+  actuator2.pos = steps2 / pulsesPerIn;
+  interrupts();
+
+  if (millis() - lastPrint >= 50) {   // 20 Hz
+    lastPrint = millis();
+    Serial.print(actuator1.pos);
+    Serial.print(",");
+    Serial.println(actuator2.pos);
   }
 }
