0a0c3b1c0653c8dbd4e620f1802a41222707cf50
FudgePizza <aviyg2@gmail.com>
2026-05-11 14:21:33 -0500

    Change server IP and enhance bandwidth metrics
    Updated server IP address and added bandwidth calculations.

--- image-reciever.py
+++ image-reciever.py
@@ -17,7 +17,7 @@ def main():
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width // 2, frame_height // 2))
 
-    HOST = "192.168.1.102"  # IPv4 Address of server computer
+    HOST = "192.168.0.64"  # IPv4 Address of server computer
     PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
     MARKER = b'IMG_END'  # Marker to indicate end of image data
 
@@ -29,6 +29,13 @@ def main():
         c.connect((HOST, PORT))
 
         print(f"Connected to server at {HOST}:{PORT}")
+
+        start_time = 0
+        frame_count = 0
+        total_size = 0
+        total_time = 0
+        maxbandwidth = 0
+
         while True:
             #  get data from socket in chunks and append to buffer until we have a complete image
             data = c.recv(262144)
@@ -50,8 +57,21 @@ def main():
                     img = cv2.resize(img, target_size)
                 out.write(img)
                 cv2.imshow('Received', img)
+
+                frame_count += 1
+                if start_time == 0:
+                    start_time = time.time()
+                imgsize = len(img_data) * 8/ (1024 * 1024) # in Mbits
+                total_size += imgsize
+                total_time = time.time() - start_time
+                bandwidth = (total_size) / total_time  # in Mbps
+                maxbandwidth = max(maxbandwidth, bandwidth)
+                fps = frame_count / total_time
+                print(f"img size: {imgsize:.4f} Mbits    fps (total): {fps:.4f} fps")
+                
                 if cv2.waitKey(1) == ord('q'):
                     break
+                    
 
     out.release()
     cap.release()
