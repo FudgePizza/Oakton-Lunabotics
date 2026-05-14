8a5194a6435b42a72dec9aa7057242bbc5eaed79
bebs-1 <101741262+bebs-1@users.noreply.github.com>
2026-05-13 20:54:32 -0500

    Add support for five video outputs and images

--- image-reciever.py
+++ image-reciever.py
@@ -16,12 +16,18 @@ def main():
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out1 = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
     out2 = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+    out3 = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+    out4 = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+    out5 = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
 
     HOST = "192.168.0.15"  # IPv4 Address of server computer
     PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
     # Markers to indicate the end of each image data
     MARKER1 = b'IMG1_END'
     MARKER2 = b'IMG2_END'
+    MARKER3 = b'IMG3_END'
+    MARKER4 = b'IMG4_END'
+    MARKER5 = b'IMG5_END'
 
     print("Waiting for connection...")
 
@@ -59,14 +65,25 @@ def main():
                 # Find indexes of markers
                 m1_idx = buffer.find(MARKER1)
                 m2_idx = buffer.find(MARKER2, m1_idx + len(MARKER1))
+                m3_idx = buffer.find(MARKER3, m2_idx + len(MARKER2))
+                m4_idx = buffer.find(MARKER4, m3_idx + len(MARKER3))
+                m5_idx = buffer.find(MARKER5, m4_idx + len(MARKER4))
+
 
                 # Use indexes to update buffers
                 img1_data = buffer[:m1_idx]
                 img2_data = buffer[m1_idx + len(MARKER1):m2_idx]
-                buffer = buffer[m2_idx + len(MARKER2):]
+                img3_data = buffer[m2_idx + len(MARKER2):m3_idx]
+                img4_data = buffer[m3_idx + len(MARKER3):m4_idx]
+                img5_data = buffer[m4_idx + len(MARKER4):m5_idx]
+
+                buffer = buffer[m5_idx + len(MARKER5):]
 
                 img1 = decode_bytes_to_image(img1_data) if len(img1_data) > 0 else None
                 img2 = decode_bytes_to_image(img2_data) if len(img2_data) > 0 else None
+                img3 = decode_bytes_to_image(img3_data) if len(img3_data) > 0 else None
+                img4 = decode_bytes_to_image(img4_data) if len(img4_data) > 0 else None
+                img5 = decode_bytes_to_image(img5_data) if len(img5_data) > 0 else None
                 
                 # ensure proper video frame sizing
                 target_size = (frame_width, frame_height)
@@ -74,18 +91,30 @@ def main():
                     img1 = cv2.resize(img1, target_size)
                 if (img2.shape[1], img2.shape[0]) != target_size:
                     img2 = cv2.resize(img2, target_size)
-                
+                if (img3.shape[1], img3.shape[0]) != target_size:
+                    img3 = cv2.resize(img3, target_size)
+                if (img4.shape[1], img4.shape[0]) != target_size:
+                    img4 = cv2.resize(img4, target_size)
+                if (img5.shape[1], img5.shape[0]) != target_size:
+                    img5 = cv2.resize(img5, target_size)
+                                
                 # display frame and write to video
                 out1.write(img1)
                 out2.write(img2)
+                out3.write(img3)
+                out4.write(img4)
+                out5.write(img5)
                 cv2.imshow('Camera 1', img1)
                 cv2.imshow('Camera 2', img2)
+                cv2.imshow('Camera 3', img3)
+                cv2.imshow('Camera 4', img4)
+                cv2.imshow('Camera 5', img5)
 
 
                 frame_count += 1
                 if start_time == 0:
                     start_time = time.time()
-                imgsize = (len(img1_data)+len(img2_data)) * 8/ (1024 * 1024) # in Mbits
+                imgsize = (len(img1_data)+len(img2_data)+len(img3_data)+len(img4_data)+len(img5_data)) * 8/ (1024 * 1024) # in Mbits
                 total_size += imgsize
                 total_time = time.time() - start_time
                 bandwidth = (total_size) / total_time  # in Mbps
@@ -99,6 +128,9 @@ def main():
             # Clean up cv2 resources
             out1.release()
             out2.release()
+            out3.release()
+            out4.release()
+            out5.release()
             cv2.destroyAllWindows()
 
             print("\nConnection closed.")
